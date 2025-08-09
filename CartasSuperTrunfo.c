90d9d17f4d3b31a697701a30e32fa7e9f3555f67
IsaiasDomingos <isaaias2009@gmail.com>
2025-08-09 21:49:29 +0000

    Primeira criacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,12 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
+
+int main(){ 
+    
+    printf("Cartas Super Trunfo\n");
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
